fdcd8529c723fdfe1497fdd806bfc463cc5429f4
ALHASSANE BARRY <albarry7991@gmail.com>
2024-06-26 07:09:39 -0700

    Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Causes an infinitive loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinitive loop incoming :(\n");
+	i = 0;
+
+	/*while (i < 10)*/
+	/*(*/
+	/*       putchar(i);*/
+	/*}*/
+
+	printf("Infinitive loop avoided \\o/\n");
+	return (0);
+}
