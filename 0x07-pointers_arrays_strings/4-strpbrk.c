34b6a9a1b7a5c0ff3e0e0acc0ffba7fba260d771
ALHASSANE BARRY <albarry7991@gmail.com>
2024-07-08 09:30:39 -0700

    password

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * _strpbrk - Entry point
  * @s: input 
  * @accept: input
- *
  * Return: Always 0 (Success)
  */
-
 char *_strpbrk(char *s, char *accept)
 {
 	int i, n;
