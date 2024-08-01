88aff3a114dd12dea424ec2cfd275764105b3119
ALHASSANE BARRY <albarry7991@gmail.com>
2024-08-01 08:41:34 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,7 +6,6 @@
   * add_node - Adds a new node at the beginning of a list
   * @head: The original linked list
   * @str: The string to add to the node
-  *
   * Return: The address of the new list or NULL if it failed
   */
 list_t *add_node(list_t **head, const char *str)
