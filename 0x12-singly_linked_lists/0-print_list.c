dfadcfa85c490a2336eede5b9bdd90b7597c659e
ALHASSANE BARRY <albarry7991@gmail.com>
2024-08-01 09:15:16 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,16 +3,15 @@
 #include <stdio.h>
 #include "lists.h"
 
-
 /**
 * print_list - check the code for Holberton School students.
 * @h: name of the list
+*
 * Return: the number of nodes.
 */
 size_t print_list(const list_t *h)
 {
    int count = 0;
-
 
    while (h)
    {
